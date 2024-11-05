37c2b2e542e053bc01ec1aacb3ab58a97d4ec815
linnaid <jiaguoyangyang@qq.com>
2024-11-05 19:22:22 +0800

    Add files via upload

---
+++ U2/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//1.
+int main()
+{
+    char name[] = "linna";
+    char surname[] = "xinyu";
+    printf("%s %s\n",name,surname);
+    printf("%s\n%s\n",name,surname);
+    printf("%s ",name);
+    printf("%s",surname);
+    return 0;
+}
---
+++ U2/2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int age,days;
+    age = 18;
+    days = age * 365;
+    printf("%d %d",age,days);
+    return 0;
+}
